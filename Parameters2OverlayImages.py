fe22baabfce6cc57ea98b064db6ad1efc2d5453d
TiainenA <Arttu.tiainen.work@gmail.com>
2024-10-01 18:36:43 +0300

    Currently it seems to work
    Quite a bit changes, but all is all the overlay maker works (at least with the test data)

--- Parameters2OverlayImages.py
+++ Parameters2OverlayImages.py
@@ -1,42 +1,61 @@
+import os
 from PIL import Image, ImageDraw, ImageFont
-
-from UDDF_telemetry import ParameterReader
-
-# Define the initiql size of the image
-width, height = 200, 100
-
-# Create a new image with RGBA mode (A stands for alpha, which controls transparency)
-image = Image.new("RGBA", (width, height), (255, 255, 255, 0))
-
-# Initialize the drawing context
-draw = ImageDraw.Draw(image)
-
-# Define the text and font
-text = "Hello, World!"
-font = ImageFont.load_default()
-
-# Calculate the width and height of the text to be drawn
-text_width, text_height = 100, 100
-
-# Calculate X, Y position of the text
-x = (width - text_width) / 2
-y = (height - text_height) / 2
-
-# Draw the text on the image
-draw.text((x, y), text, font=font, fill=(255, 255, 255, 255))
-
-# Save the image
-image.save("text_image.png") """
+from UDDF_telemetry import UDDF_Reader,ParameterReader
+import matplotlib.colors
 
 
 
-def TextDrawer():
+def kuvain(Parameters,DiveInfo, UsedColumns, DFSlice):
+    #A in RGBA is the opacity (why it isn't RGBO?)
+    image = Image.new("RGBA", (int(Parameters["ResolutionX"]), int(Parameters["ResolutionY"])), (255, 255, 255, 0))
+   
+
+
+    font = ImageFont.truetype("arial.ttf", int(Parameters["FontSize"]))
+
+    # Initialize the drawing context
+    draw = ImageDraw.Draw(image)
+    #This goes through the parameters and draws them to the screen one by one
+    for column in UsedColumns:
+        if column == "alarm" and str(DFSlice[Parameters[column+"Variable"]])=="nan":
+            continue
+        if column == "tankpressure":
+            Variable=int(DFSlice[Parameters[column+"Variable"]])/10000
+            unit=Parameters[column+"Unit"]
+        elif column == "temperature":
+            Variable=int(DFSlice[Parameters[column+"Variable"]])-273
+            unit= '\xb0 C'
+        else:
+            Variable=DFSlice[Parameters[column+"Variable"]]
+            unit=Parameters[column+"Unit"]
+        #Determining text, location, and color
+        text = f"{Parameters[column+"Text"]} {Variable} {unit}"
+        
+        x = int(Parameters[column+"X"])/100*int(Parameters["ResolutionX"])
+        y = int(Parameters[column+"Y"])/100*int(Parameters["ResolutionY"])
+
+
+        textColor=matplotlib.colors.to_rgba_array(Parameters[column+"Color"])[0].tolist()
+        
+        textOpacity=255*int(Parameters[column+"Opacity"])
+
+
+        for i in range(len(textColor)):
+            textColor[i-1]=255*int(textColor[i-1])
+
+        draw.text((x, y), text, font=font, fill=(textColor[0], textColor[1], textColor[2], textOpacity))
+    
+    filename=f"{DiveInfo["datetime"].replace(":","")}"
+    image.save(f"{os.getcwd()+Parameters.get("OutputPath")}\\{filename}_DT_{DFSlice['ns1:divetime']}.png") 
 
 
 def main():
-    
-    Parameters=ParameterReader()
-
+    BasicParameters = ParameterReader()
+    DiveData,DiveInfo,InterestingParameters=UDDF_Reader()
+    for index,row in DiveData.iterrows():
+        #print(row['ns1:divetime'], row['ns1:depth'],row['ns1:alarm'])
+        kuvain(BasicParameters, DiveInfo, InterestingParameters, row)   
 
 if __name__ == "__main__":
     main()
+ 
--- UDDF_telemetry.py
+++ UDDF_telemetry.py
@@ -1,9 +1,10 @@
 import os
-#from pyautogui import typewrite
+from datetime import datetime
+
 import pandas as pd
 import csv
 import UDDF_CSV_Reader
-
+import Parameters2OverlayImages
 
 
 def ParameterReader():
@@ -12,9 +13,12 @@ def ParameterReader():
         mydict = {rows[0]:rows[1] for rows in reader}
     return(mydict)
 
-def main():
+def UDDF_Reader():
+
     #To get all parameters in the settings
     Parameters=ParameterReader()
+    DiveInfo={}
+
     UsedParams=[]
     for key in Parameters:
         if Parameters[key]=="TRUE":
@@ -23,14 +27,7 @@ def main():
     ParamVariables=[]
     for key in UsedParams:
         ParamVariables.append(Parameters.get(key+"Variable"))
-
-
-
-    #df = pd.read_csv("C:\\Users\\User\\github\\UDDF-to-telemetry-overlay\\Data\\2024-09-22 Dive1.csv", sep=';')
-
-
-
-    
+   
 
     
     InputVar=Parameters.get("DefaultFolder")
@@ -62,14 +59,53 @@ def main():
 
 
     df=UDDF_CSV_Reader.TelemetryReader(path+FileList[FileNumber])
+    
+    #Set Dive info, such as date and location
+    DiveInfoKeys=['datetime','location','latitude','longitude']
+    DiveInfoColumns=['ns1:datetime','ns1:name12','ns1:latitude','ns1:longitude']
+    for i in range(len(DiveInfoKeys)):
+
+        if DiveInfoColumns[i-1] in df.columns.tolist():
+             DiveInfo[DiveInfoKeys[i-1]]=df[DiveInfoColumns[i-1]].iloc[0]
+
+    #To convert the time format
+    
+    DiveInfo["datetimeISO"] = datetime.strptime(DiveInfo["datetime"], "%d.%m.%Y %H:%M").isoformat()
+    
+    
     #Forward filling option 
     if Parameters.get("MissingValues")=="ffill":
-        df=df.ffill(limit=50)
+        #df=df.ffill(limit=50)
+        df=df.interpolate()
+        
+    #Removing NAN values in ns1:divetime from the beginning
+    df = df[df['ns1:divetime'].notna()]
+    #Removes unusable parameters from that particular dive (for example if no air integration and it was desired)
+    RemovedVariables=[]
+    for i in UsedParams:
+        if Parameters[i+"Variable"] in df.columns.tolist():
+            continue
+        else:
+            RemovedVariables.append(i)
+    if len(RemovedVariables) != 0:
+        for i in RemovedVariables:
+            UsedParams.remove(i)
+        print(f"Removed following parameters {RemovedVariables}")
+
 
     #Dropping all other variables
     FilteredDF=df.filter(ParamVariables,axis=1)
-  
 
+    return(FilteredDF, DiveInfo,UsedParams)    
+
+
+
+def main():
+    BasicParameters = ParameterReader()
+    DiveData,DiveInfo,InterestingParameters=UDDF_Reader()
+    for index,row in DiveData.iterrows():
+        #print(row['ns1:divetime'], row['ns1:depth'],row['ns1:alarm'])
+        Parameters2OverlayImages.kuvain(BasicParameters, DiveInfo, InterestingParameters, row)  
 if __name__ == "__main__":
     main()
 
